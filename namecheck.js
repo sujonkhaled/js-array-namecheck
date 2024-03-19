363683a07549be8108496cc42bff11bcfe91ea5e
KHALED MAHMUD SUJON <86775777+sujonkhaled@users.noreply.github.com>
2024-03-19 11:00:11 +0800

    Createnamecheck.js

---
+++ namecheck.js
@@ -0,0 +1,10 @@
+var name = prompt("kindly enter your name ");
+var check = ["khaled", "kabir" , "babul"];
+var isinclude= check.includes(name);
+if(isinclude){
+ alert("hi  " + name + " welcome ");
+
+}
+else {
+ alert("sorry try again ");
+}
